6f5b40bb6a2dfeca296bd2a3cba0b7e72d4a80b4
rkommineni <rupasravanikommineni@gmail.com>
2014-09-03 13:24:21 -0500

    Update wrapper.sh

--- wrapper.sh
+++ wrapper.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#ptu.tar contains the built PTU tool. Untar and add to PATH to use it.
+
 wget http://stash.osgconnect.net/~rkommineni/ptu.tar
 tar xvf ptu.tar
 
 export PATH=$PWD/ptu:$PATH
+
+#Untar the package to re-execute.
 
 tar xvf reco.tar
 cd ptu-Reco
